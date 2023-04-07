3fc95d389ecf05ac3329a83cf062b1bad2289c20
simleo <simleo@crs4.it>
2023-04-07 14:55:26 +0200

    add test for runcrate run

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import Path
+
 from click.testing import CliRunner
 from rocrate.rocrate import ROCrate
 
@@ -107,3 +109,16 @@ def test_cli_version():
     result = runner.invoke(cli, ["version"])
     assert result.exit_code == 0
     assert result.stdout.strip() == __version__
+
+
+def test_cli_run_provenance_minimal(data_dir, tmpdir, monkeypatch):
+    crate_dir = data_dir / "revsort-provenance-crate-minimal"
+    runner = CliRunner()
+    args = ["run", str(crate_dir)]
+    monkeypatch.chdir(str(tmpdir))
+    result = runner.invoke(cli, args)
+    assert result.exit_code == 0
+    out_path = Path("output.txt")
+    assert out_path.is_file()
+    crate_out_path = crate_dir / "b9214658cc453331b62c2282b772a5c063dbd284"
+    assert out_path.read_text() == crate_out_path.read_text()
