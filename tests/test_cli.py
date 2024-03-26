cbfd4c0f87019a0c2701882c4ba3fe10ed227365
daniel <github@dbauer.me>
2024-03-26 12:08:50 +0100

    update copyright with SGN

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 # Copyright 2022-2024 CRS4.
+# Copyright 2024 Senckenberg Society for Nature Research
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
